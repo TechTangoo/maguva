357d97984c39578cb9a3bba79d216d498c604823
priyankaj04 <priyankaj2002o4@gmail.com>
2023-12-23 21:40:00 +0530

    added responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,22 +7,22 @@ export default function About() {
   };
 
   return (
-    <div id='about' className="flex flex-row items-center justify-between p-12 md:p-24">
-      <div className='flex flex-col max-w-lg flex-1'>
-        <h2 className='font-palanquin text-4xl font-bold text-amber-500 mb-4 lg:max-w-lg' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 800 }}>Welcome to Maguva</h2>
-        <p className='text-amber-800 text-xl font-bold mb-6 italic'>Where Health Meets Taste</p>
-        <p className='text-gray-600 leading-7' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 800 }}>
+    <div id='about' className="flex flex-col lg:flex-row flex-wrap items-center justify-between p-12 md:p-24">
+      <div className='flex flex-col flex-1'>
+        <h2 className='font-palanquin lg:text-5xl xl:text-5xl text-center text-4xl font-bold text-amber-500 mb-4' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 800 }}>Welcome to Maguva</h2>
+        <p className='text-amber-800 lg:text-2xl xl:text-2xl text-center text-xl font-bold mb-6 italic'>Where Health Meets Taste</p>
+        <p className='text-gray-900 lg:text-xl xl:text-xl leading-7' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 800 }}>
           At Maguva, we believe in the power of nature to enrich our lives and enhance our well-being.
           We are passionate about providing you with the finest quality powdered products that not only
           taste great but also contribute to a healthier lifestyle.
         </p>
-        <p className='text-gray-600 leading-7 mt-4' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 800 }}>
+        <p className='text-gray-900 lg:text-xl xl:text-xl leading-7 mt-4' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 800 }}>
           <span className='font-bold' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 800 }}>Our Promise:</span> We are more than just a brand; we are a community that shares a love for good food and a commitment to well-being.
           When you choose Maguva, you're not just buying a product - you're joining us on a journey to discover the endless possibilities of flavor and nutrition.
         </p>
       </div>
       <div className='flex-1 items-center m-3'>
-        <img src={spiceMixImage} alt='Masale Spice Mix' className='max-w-full rounded-md shadow-lg' />
+        <img src={spiceMixImage} alt='Masale Spice Mix' className='rounded-md shadow-lg' />
       </div>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,22 @@ import heroImage from '../assets/heroimage.jpg'
 
 export default function Home() {
   return (
-    <div id='home' className="flex flex-col">
+    <div id='home' className="flex flex-col flex-1 w-screen">
       <div className="">
-        <div style={{ background: `url(${heroImage})`, backgroundRepeat: 'no-repeat', display: 'flex', flex: 1, backgroundSize: 'cover', height: 600, alignItems: 'center' }} className='h-5/6'>
+        <div style={{
+          background: `url(${heroImage})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center', // Center the background image
+          display: 'flex',
+          flex: 1,
+          minHeight: '600px', // You can adjust this value based on your design
+          alignItems: 'center',
+        }}
+          className='h-5/6 w-screen'>
           <div className='flex flex-1 justify-center flex-col'>
-            <p className='text-7xl text-amber-300 text-center' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 1000 }}>Authentic Andra Powder</p>
-            <p className='text-2xl text-amber-200 text-center' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 600 }}>Homemade masala powders<subscript>®</subscript></p>
+            <p className='text-5xl lg:text-7xl text-amber-300 text-center' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 1000 }}>Authentic Andra Powder</p>
+            <p className='text-xl lg:text-2xl text-amber-200 text-center' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 600 }}>Homemade masala powders<subscript>®</subscript></p>
           </div>
         </div>
       </div>
