895003d7e91c5506894646d098126fba0c0f5a5e
priyankaj04 <priyankaj2002o4@gmail.com>
2023-12-23 17:37:11 +0530

    added color changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,12 @@ import heroImage from '../assets/heroimage.jpg'
 
 export default function Home() {
   return (
-    <div id='home' className="flex min-h-screen flex-col">
+    <div id='home' className="flex flex-col">
       <div className="">
         <div style={{ background: `url(${heroImage})`, backgroundRepeat: 'no-repeat', display: 'flex', flex: 1, backgroundSize: 'cover', height: 600, alignItems: 'center' }} className='h-5/6'>
           <div className='flex flex-1 justify-center flex-col'>
-            <p className='text-5xl font-bold text-white text-center'>Authentic Andra Powder</p>
-            <p className='text-xl text-white text-center'>Homemade masala powders<subscript>®</subscript></p>
+            <p className='text-7xl text-amber-300 text-center' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 1000 }}>Authentic Andra Powder</p>
+            <p className='text-2xl text-amber-200 text-center' style={{ fontFamily: 'Nunito, sans-serif', fontWeight: 600 }}>Homemade masala powders<subscript>®</subscript></p>
           </div>
         </div>
       </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -52,7 +52,7 @@ const ProductCard = () => {
       {products.map((product, index) => (
         <div
           key={index}
-          className="border flex-shrink-0 border-gray-300 p-4 rounded cursor-pointer hover:shadow-md transition duration-300"
+          className="border flex-shrink-0 border-gray-300 p-4 rounded cursor-pointer shadow-xl transition duration-300"
           onClick={() => handleButtonClick(product)}
         >
           {product.src && (
@@ -63,42 +63,37 @@ const ProductCard = () => {
               style={{ maxWidth: '100%', maxHeight: '300px', height: 'auto' }}
             />
           )}
-          <strong className="font-bold text-gray-800">{product.name}</strong>
-          <p className="text-gray-600">{/* ... (product description or other details) ... */}</p>
         </div>
       ))}
       {showModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-6 rounded w-2/3">
             <div className="flex justify-between items-center mb-4">
-              <h2 className="font-bold text-xl">{selectedProduct.name}</h2>
-              <button
-                className="bg-gray-500 text-white px-3 py-1 rounded hover:bg-gray-700"
-                onClick={handleModalClose}
-              >
-                Close
-              </button>
+              <h2 className="font-bold text-xl text-amber-600" style={{fontWeight: 800, fontFamily:'Nunito, sans-serif', fontSize: 30}}>{selectedProduct.name}</h2>
+              <div onClick={handleModalClose} className='cursor-pointer text-2xl'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg></div>
             </div>
             <div className="flex gap-4">
               <div className="flex-shrink-0">
                 {selectedProduct.iframelink && (
-                <iframe
-                title=""
-                allowFullScreen
-                style={{ border: 'none', width: '500px', height: '500px' }}
-                src={selectedProduct.iframelink}
-              />
+                  <iframe
+                    title=""
+                    allowFullScreen
+                    style={{ border: 'none', width: '500px', height: '500px' }}
+                    src={selectedProduct.iframelink}
+                  />
                 )}
               </div>
               <div className="flex flex-col">
-                <p className="font-bold mb-3">Health Benefits</p>
+                <p className="font-bold mb-3 text-amber-700" style={{ fontWeight: 800, fontFamily: 'Nunito, sans-serif', fontSize: 24 }}>Health Benefits</p>
                 <ul className="list-disc pl-4 mb-3">
                   {selectedProduct.health.split(',').map((benefit, index) => (
-                    <li key={index}>{benefit.trim()}</li>
+                    <li key={index} style={{ fontWeight: 600, fontFamily: 'Nunito, sans-serif', fontSize: 18}}>{benefit.trim()}</li>
                   ))}
                 </ul>
-                <p className="mb-3">Ingredients used: {selectedProduct.ingredient}</p>
-                <label htmlFor="packets" className="mr-2">
+                <p className="mb-3" style={{ fontWeight: 600, fontFamily: 'Nunito, sans-serif', fontSize: 18 }}>Ingredients used: {selectedProduct.ingredient}</p>
+                <label htmlFor="packets" className="mr-2 text-amber-700" style={{ fontWeight: 800, fontFamily: 'Nunito, sans-serif', fontSize: 20 }}>
                   No. of Packets:
                 </label>
                 <input
@@ -111,7 +106,7 @@ const ProductCard = () => {
                   className="border px-2 py-1 mb-3"
                 />
                 <button
-                  className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700"
+                  className="bg-amber-500 text-white px-4 py-2 rounded hover:bg-amber-700"
                   onClick={handleAddToCart}
                 >
                   Add to Cart
